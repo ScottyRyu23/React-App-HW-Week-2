d2b28ef11baf081a6e03bd74ec4735924728d564
Scott Roberson <srkazan23@gmail.com>
2018-04-22 21:20:19 -0500

    React Home Work

---
+++ src/components/blog.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class Blog extends  React.Component {
+   
+   constructor(props){
+       super(props);
+
+       this.state = {
+           title: '',
+           author: '',
+           body:''
+       }
+   }
+   
+   
+   
+   
+   
+    render (){
+        return (
+            <div>
+                <h1> Blog-o-sphere</h1>
+                 <div>
+                  <form action="">
+                       <label htmlFor="">
+                           Title:
+                           <input value={this.state.title}  type="text" name="title"/>    
+                       </label> 
+                       <label htmlFor="">
+                           Author:
+                           <input type="text" name= "author"/>
+                       </label>
+
+                       <textarea name="body" id="" cols="30" rows="10">
+
+                       </textarea>        
+                        <input type="submit" value="submit"/>
+                 </form>    
+                </div>
+            
+            </div>
+        );
+    }
+}
